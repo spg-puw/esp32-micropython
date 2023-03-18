797be1f38ba53caa9aa8116397dbe51c2d95ef7e
spg-puw <117909782+spg-puw@users.noreply.github.com>
2023-03-18 18:49:30 +0100

    fix servo

--- espcode/demos/04_actor/servo/servo_simple.py
+++ espcode/demos/04_actor/servo/servo_simple.py
@@ -1,22 +1,22 @@
 from machine import Pin, PWM
 import time
 
-servo1 = PWM(Pin(18), freq=50, duty=0)
+# Verbindungen Servo
+# Braun = GND
+# Rot = Vin / 5V
+# Orange = Signal = Pin D13
+servo1 = PWM(Pin(13), freq=50, duty=0)
 
-def Servo(servo,angle):
-    servo1.duty(int(((angle+90)*2/180+0.5)/20*1023))
+def angle2duty(angle):
+    # von 0 bis 180
+    minDutyValue = 102
+    maxDutyValue = 204    
+    calculatedDutyValue = int(minDutyValue + ((angle/180) * (maxDutyValue-minDutyValue)))
+    r = max(minDutyValue, min(calculatedDutyValue, maxDutyValue))
+    print(r)
+    return r
 
-Servo(servo1,-90)
-time.sleep(1)
-
-Servo(servo1,-45)
-time.sleep(1)
-
-Servo(servo1,0)
-time.sleep(1)
-
-Servo(servo1,45)
-time.sleep(1)
-
-Servo(servo1,90)
-time.sleep(1)
+for i in range(6):
+    print("Position {i}".format(i=i))
+    servo1.duty(angle2duty(i*36))
+    time.sleep(1)
