243efbbc10ded8dceca6141665744c2986565131
spg-puw <117909782+spg-puw@users.noreply.github.com>
2023-10-28 00:22:47 +0200

    fix isr sleep

--- espcode/demos/01_basics/gpio/PushButton_Lichtschalter_irq_debounced.py
+++ espcode/demos/01_basics/gpio/PushButton_Lichtschalter_irq_debounced.py
@@ -1,16 +1,31 @@
-from machine import Pin
+import machine
 import time
 
-led1 = Pin(2, Pin.OUT)
-button1 = Pin(0, Pin.IN, Pin.PULL_UP)
+led1 = machine.Pin(2, machine.Pin.OUT)
+button1 = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
 
 state = False
+buttonHasBeenPressed = False
 
-def irq_pushButton(pin):
+def irq_pushButton_bad(pin):
     global state
-    time.sleep_ms(10) # 10ms warten (debounce)
-    if pin.value() == 0:
-        state = not state
-        led1.value(state)
+    time.sleep_ms(50) # 50ms warten (debounce) - ACHTUNG: sleep im IRQ ist schlecht!
+    state = not state
+    led1.value(state)
 
-button1.irq(irq_pushButton, Pin.IRQ_FALLING)
+button1.irq(irq_pushButton_bad, machine.Pin.IRQ_FALLING) # irq bei fallender Flanke
+
+def irq_pushButton_better(pin):
+    # move processing and debouncing to main loop
+    global buttonHasBeenPressed
+    buttonHasBeenPressed = True
+
+# button1.irq(irq_pushButton_better, machine.Pin.IRQ_FALLING) # irq bei fallender Flanke
+
+while True:    
+    if buttonHasBeenPressed == True:
+        time.sleep_ms(50) # warten
+        state = not state
+        buttonHasBeenPressed = False
+        
+    led1.value(state)
