a61854bb83c58574b1f8f015ff2e94f2e4adf540
spg-puw <117909782+spg-puw@users.noreply.github.com>
2023-10-28 19:11:14 +0200

    add description

--- espcode/demos/03_sensor/ultrasonic/ultrasonic_hcsr04.py
+++ espcode/demos/03_sensor/ultrasonic/ultrasonic_hcsr04.py
@@ -1,9 +1,20 @@
-from hcsr04 import HCSR04
+'''
+Verkabelung KY-050 (Ultraschall-Abstandssensor)
+KY-050 ... ESP32
+Vcc        Vin
+Trig       D18
+Echo       D19
+Gnd        GND
+
+Anmerkung: bitte 5V (Vin-Pin bei USB) verwenden!
+'''
+from hcsr04 import HCSR04 # see lib/hcsr04.py
 import time
 
-# bitte 5V (VIN-Pin bei USB) verwenden!
-
 sensor = HCSR04(trigger_pin=18, echo_pin=19)
+
+print("Use the plotter in Thonny: View -> Plotter")
+
 while True:
     d = sensor.distance_cm()
     print(d)
