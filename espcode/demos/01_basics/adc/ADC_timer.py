8824a5bbb2a8382286bdf1e2a0fec558300ad4f8
spg-puw <117909782+spg-puw@users.noreply.github.com>
2023-10-29 01:19:37 +0200

    fix reads

--- espcode/demos/01_basics/adc/ADC_timer.py
+++ espcode/demos/01_basics/adc/ADC_timer.py
@@ -2,10 +2,10 @@ import machine
 
 adc1 = machine.ADC(machine.Pin(32))
 
-def timer_callback_adc(t):
-     print(str('%.2f'%((adc1.read()/4095) * 0.95)) + "V")
+def timerCallback(t):
+    print("Wert 12bit: {0}   Wert 16bit: {1}   Spannung: {2} V".format(adc1.read(), adc1.read_u16(), adc1.read_uv()/1000000))
 
 timer1 = machine.Timer(-1)
-timer1.init(period=300, mode=machine.Timer.PERIODIC, callback=timer_callback_adc)
+timer1.init(period=300, mode=machine.Timer.PERIODIC, callback=timerCallback)
 
 # Achtung: Timer muss deaktiviert werden (Reset oder Ctrl+D soft reboot)
