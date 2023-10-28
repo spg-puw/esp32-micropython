99613b75b22c65025e1d6b6f71434fee62b7eb2a
spg-puw <117909782+spg-puw@users.noreply.github.com>
2023-10-28 16:35:55 +0200

    add description

--- espcode/demos/04_actor/buzzer/PWM_beep.py
+++ espcode/demos/04_actor/buzzer/PWM_beep.py
@@ -1,7 +1,19 @@
-from machine import Pin, PWM
+'''
+Verbindungen
+Buzzer (KY-006) ... ESP
+-                   GND
+S                   D25
+'''
+
+import machine
 import time
 
-beep1 = PWM(Pin(25), freq=0, duty=512)
+# PWM signal starts with 1 Hz, you should hear a clicking
+beep1 = machine.PWM(machine.Pin(25), freq=1, duty_u16=2**16//2)
+time.sleep(3)
+
+beep1.freq(100)
+time.sleep_ms(1000)
 
 beep1.freq(200)
 time.sleep_ms(1000)
@@ -9,13 +21,11 @@ time.sleep_ms(1000)
 beep1.freq(400)
 time.sleep_ms(1000)
 
-beep1.freq(600)
-time.sleep_ms(1000)
-
 beep1.freq(800)
 time.sleep_ms(1000)
 
-beep1.freq(1000)
+beep1.freq(1600)
 time.sleep_ms(1000)
 
+# stop the PWM signal - without deinit the signal will continue after program exits
 beep1.deinit()
