a2aab90398766ad14dafd87256738e9de87858a1
spg-puw <117909782+spg-puw@users.noreply.github.com>
2023-10-28 17:01:11 +0200

    add button

--- espcode/demos/03_sensor/joystick/adc_joystick.py
+++ espcode/demos/03_sensor/joystick/adc_joystick.py
@@ -1,3 +1,13 @@
+'''
+Verbindung von KY-023 (Joystick)
+Joystick ... ESP
+GND          GND
++5V          Vin
+VRx          D34
+VRy          D35
+SW           D32
+'''
+
 import machine
 import time
 
@@ -5,6 +15,7 @@ import time
 
 joyX = machine.Pin(34, machine.Pin.IN)
 joyY = machine.Pin(35, machine.Pin.IN)
+joyButton = machine.Pin(32, machine.Pin.IN, machine.Pin.PULL_UP)
 
 joyXadc = machine.ADC(joyX)
 joyXadc.atten(machine.ADC.ATTN_11DB) #Lesebereich 0,15 bis 2,5 V
@@ -14,6 +25,6 @@ joyYadc.atten(machine.ADC.ATTN_11DB) #Lesebereich 0,15 bis 2,5 V
 while True:
     valX = joyXadc.read()  #unsigned integer mit range 0-4095
     valY = joyYadc.read()  #unsigned integer mit range 0-4095
-    print("X Wert: {x}      Y-Wert: {y}".format(x=valX, y=valY))
+    btn = not joyButton.value()
+    print("X Wert: {x}      Y-Wert: {y}    Button: {b}".format(x=valX, y=valY, b=btn))
     time.sleep(0.5) #0.5 sekunden
-    
