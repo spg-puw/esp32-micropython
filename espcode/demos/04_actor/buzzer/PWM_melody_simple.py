0badd3b59979834dade1bf4ec46d4fba9ad3a35f
spg-puw <117909782+spg-puw@users.noreply.github.com>
2023-10-28 16:36:39 +0200

    add description, german tones, simpler code, pause

--- espcode/demos/04_actor/buzzer/PWM_melody_simple.py
+++ espcode/demos/04_actor/buzzer/PWM_melody_simple.py
@@ -1,23 +1,39 @@
-from machine import Pin, PWM
-from time import sleep
-buzzer = PWM(Pin(25, Pin.OUT), freq=0, duty=512)
+'''
+Verbindungen
+Buzzer (KY-006) ... ESP
+-                   GND
+S                   D25
+'''
+
+import machine
+import time
+
+buzzer = machine.PWM(machine.Pin(25, machine.Pin.OUT), freq=1, duty_u16=2**16//2)
 tones = {
-    'c': 262,
+    'c': 262, # c1
     'd': 294,
     'e': 330,
     'f': 349,
     'g': 392,
-    'a': 440,
-    'b': 494,
-    'C': 523,
-    ' ': 0,
+    'a': 440, # a1
+    'h': 494,
+    'C': 523, # c2
+    ' ': 0,   # pause
 }
-melody = 'cdefg g aaaag aaaag ffffee ddddc'
-rhythm = [8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8]
-tempo = 5
-for tone, length in zip(melody, rhythm):
-    print(tone)
-    buzzer.freq(tones[tone])
-    sleep(tempo/length)
+
+melody = 'cdefgahC cdefg g aaaag   aaaag   ffffe e ddddc'
+tempo = 0.5
+
+for tone in melody:
+    frequency = tones[tone]
+    print("{0} has a frequency of {1} Hz".format(tone, frequency))
+    if frequency > 0:
+        buzzer.init(frequency)
+        
+    time.sleep(tempo)
+    
+    # short pause after tone
+    buzzer.deinit()
+    time.sleep(0.01)
 
 buzzer.deinit()
