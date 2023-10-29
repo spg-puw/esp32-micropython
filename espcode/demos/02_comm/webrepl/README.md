064a5e58e4b1b8503373246566c6e910911e25cf
spg-puw <117909782+spg-puw@users.noreply.github.com>
2023-10-29 02:52:13 +0200

    add webrepl

---
+++ espcode/demos/02_comm/webrepl/README.md
@@ -0,0 +1,3 @@
+# WebREPL
+
+siehe [hier](https://docs.micropython.org/en/latest/esp32/quickref.html#webrepl-web-browser-interactive-prompt)
---
+++ espcode/demos/02_comm/webrepl/webrepl.py
@@ -0,0 +1,9 @@
+print("please open your shell and follow the setup guide ...")
+
+import webrepl_setup
+
+# in your boot.py:
+# import webrepl
+# webrepl.start()
+# webrepl.start(password='secret') # or start with a specific password
+# the password may also be stored in webrepl_cfg.py
